af8c31bec8fc97d836b145475556b29f9612926c
Mausumi Acharya <mausumi.acharya@in.ibm.com>
2025-03-09 22:56:06 +0530

    Add files via upload

---
+++ Python/hello.py
@@ -0,0 +1,17 @@
+print("Hello, World!")
+
+b = 'Hello, World!'
+print (b[2:5])
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a) 
+
+
+a = 0
+
+while a < 5:
+  print(a)
+  a += 1
