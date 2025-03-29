6cd29875fc375059d0fa1ea2bee582e131cd8548
Mausumi Acharya <mausumi.acharya@in.ibm.com>
2025-03-29 20:29:11 +0530

    Update Activity2.java

--- Appium/Project/Activity2.java
+++ Appium/Project/Activity2.java
@@ -1,75 +1,90 @@
-package liveProject;
+package projects;
 
+import static org.testng.Assert.assertEquals;
 import static activities.ActionsBase.longPress;
-
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
 import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.time.Duration;
 
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.Point;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import com.google.common.collect.ImmutableMap;
+
 import io.appium.java_client.AppiumBy;
+import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 
 public class Activity2 {
-	// Declare driver
-	AndroidDriver driver;
+	AppiumDriver driver;
 	WebDriverWait wait;
-
-	// Setup method
 	@BeforeClass
-	public void setUp() throws MalformedURLException {
-		// Desired Capabilities
-		UiAutomator2Options caps = new UiAutomator2Options();
-		caps.setPlatformName("android");
-		caps.setAutomationName("UiAutomator2");
-		caps.setAppPackage("com.app.todolist");
-		caps.setAppActivity(".view.MainActivity");
-		caps.noReset();
-
-		// Appium Server URL
-		URL serverURL = new URL("http://localhost:4723");
-
-		// Initialization of driver
-		driver = new AndroidDriver(serverURL, caps);
-		// Initialization of wait
+	public void setup() throws MalformedURLException, URISyntaxException {
+		
+		UiAutomator2Options options = new UiAutomator2Options()
+				.setPlatformName("android")
+				.setAutomationName("UiAutomator2")
+				.setAppPackage("com.app.todolist")
+				.setAppActivity(".view.MainActivity")
+				.noReset();
+		
+		URL serverURL = new URI("http://127.0.0.1:4723").toURL();
+		
+		driver = new AndroidDriver(serverURL,options);
+		
 		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 	}
 
+	
+	
 	@Test
-	public void tasksTest2() {
-		// Get the size of the screen
+	public void editTask() {
+		
+		WebElement firstTask = driver.findElement(By.xpath("(//android.widget.RelativeLayout[@resource-id=\"com.app.todolist:id/rl_exlv_task_group_root\"])[1]"));
+		/*
+		//using JS executor for longpress
+		((JavascriptExecutor)driver).executeScript("mobile:longClickGesture", 
+				ImmutableMap.of("elementId",((RemoteWebElement)firstTask).getId()
+				,"duration",2000));
+				*/
+		//using point
 		Dimension dims = driver.manage().window().getSize();
-		// Set the point of long press
-		Point start = new Point((int) (dims.getWidth() * .50), (int) (dims.getHeight() * .15));
-		
-		// Perform long press on the first list item
+		Point start = new Point((int) (dims.getWidth() * .70), (int) (dims.getHeight() * .15));
 		longPress(driver, start);
-		// Select edit task
-		driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text='Edit To-Do Task']")).click();
 		
-		// Set a deadline for the coming weekend
-		driver.findElement(AppiumBy.id("tv_todo_list_deadline")).click();
-		driver.findElement(AppiumBy.accessibilityId("15 February 2025")).click();
-		// Save details
-		driver.findElement(AppiumBy.id("bt_deadline_ok")).click();
-		driver.findElement(AppiumBy.id("bt_new_task_ok")).click();
-
-		// Assertions
-		String expectedDeadline = driver.findElement(AppiumBy.id("tv_exlv_task_deadline")).getText();
-		Assert.assertEquals(expectedDeadline, "Deadline: 15.02.2025");
+		//clicking to do
+		driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text=\"Edit To-Do Task\"]")).click();
+		//deadline ele
+		driver.findElement(AppiumBy.id("com.app.todolist:id/tv_todo_list_deadline")).click();
+		//selecting date
+		wait.until(ExpectedConditions.elementToBeClickable(AppiumBy.accessibilityId("29 March 2025"))).click();
+		//clicking ok
+		driver.findElement(AppiumBy.id("com.app.todolist:id/bt_deadline_ok")).click();
+		
+		wait.until(ExpectedConditions.textToBePresentInElementLocated(AppiumBy.id("com.app.todolist:id/dialog_edit"), "Edit To-Do task"));
+		//clicking final Ok
+		driver.findElement(AppiumBy.id("com.app.todolist:id/bt_new_task_ok")).click();
+		//wait.until(ExpectedConditions.visibilityOf(firstTask));
+		String deadLineDate = driver.findElement(By.xpath("//android.widget.TextView[@text=\"Deadline: 29.03.2025\"]")).getText();
+		assertEquals(deadLineDate, "Deadline: 29.03.2025");
 	}
-
+	
 	@AfterClass
 	public void tearDown() {
-		// Close the app
 		driver.quit();
 	}
+
 }
